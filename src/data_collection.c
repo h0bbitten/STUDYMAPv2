c826cf7a923013ad1d5007656d4de88996b1b4b3
allankrog <mq75eh@student.aau.dk>
2023-11-29 08:21:50 +0100

    Hashed password works!!!

--- src/data_collection.c
+++ src/data_collection.c
@@ -5,14 +5,27 @@
 #include <string.h>
 #include <stdbool.h>
 
+
+
 Logins current_user;
+
+void hash(char *str, unsigned int *result) {
+    *result = 0;
+
+    while (*str) {
+        *result = (*result << 5) + *str++;
+    }
+}
+
 
 void login()
 {
     //Creates a user of the type struct Logins and inputs the current user into it
     current_user = load_user();
+    unsigned int hashed_password;
+    hash(current_user.password, &hashed_password);
     //debug line
-    printf("Username: %s, Password:  %s, CPR: %s",current_user.username, current_user.password, current_user.cpr);
+    printf("Username: %s, Password:  %u, CPR: %s",current_user.username, hashed_password, current_user.cpr);
 }
 
 Logins load_user()
@@ -75,8 +88,10 @@ Logins load_user()
 
         fclose(Users);
 
+        unsigned int hashed_password;
+
         printf("Please enter a password that is no longer than %d characters\n>", PASSWORD_MAX_LENGTH);
-        scanf("%s", this_user.password);
+        scanf("%s", &hashed_password);
 
         printf("Please enter your CPR-number\n>");
         scanf("%s", this_user.cpr);
@@ -90,7 +105,7 @@ Logins load_user()
             exit(1);
         }
 
-        fprintf(Users, "%s,%s,%s\n", this_user.username, this_user.password, this_user.cpr);
+        fprintf(Users, "%s,%u,%s\n", this_user.username, hashed_password, this_user.cpr);
         fclose(Users);
 
         return this_user;
@@ -99,13 +114,14 @@ Logins load_user()
     if (y_n == 'L' || y_n == 'l') {
         bool found_username = false;
         bool found_password = false;
+        unsigned int hashed_password;
 
         do {
             printf("Please enter your username\n>");
             scanf(" %s", this_user.username);
 
             printf("Please enter your password\n>");
-            scanf(" %s", this_user.password);
+            scanf(" %s", &hashed_password);
 
             char line[MAX_LINE_LENGTH];
 
@@ -116,7 +132,7 @@ Logins load_user()
                 if (username != NULL && strcmp(username, this_user.username) == 0) {
                     found_username = true;
                     char *password = strtok(NULL, ",");
-                    if (password != NULL && strcmp(password, this_user.password) == 0) {
+                    if (password != NULL && atoi(password) == hashed_password) {
                         found_password = true;
                         fclose(Users);
                         break;
