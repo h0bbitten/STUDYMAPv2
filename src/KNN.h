90c0e1639160093fc8aec74cd398d60f1a5e1ee0
FalseViking <sorenbks@gmail.com>
2023-11-30 11:54:46 +0100

    KNN-rework

--- src/KNN.h
+++ src/KNN.h
@@ -1,14 +1,39 @@
-// KNN.h
-#include <math.h>
+#ifndef KNN_HEADER_H
+#define KNN_HEADER_H
 
-#ifndef STUDYMAP_KNN_H
-#define STUDYMAP_KNN_H
+#include <inttypes.h>
 
-// Declare the functions here
-double calculateDistance(double *point1, double *point2, int numFeatures);
-int findMinIndex(double *arr, int size);
-int knn(double **trainingData, double *newData, int numTrainingSamples, int numFeatures);
-double **readTrainingData(const char *filename, int *numTrainingSamples, int *numFeatures);
-void freeTrainingData(double **trainingData, int numTrainingSamples);
+#define DATATYPE double
 
-#endif /* STUDYMAP_KNN_H */
+// Structure to represent a sample with its dimensions, group, and temporary distance
+struct Sample {
+    DATATYPE *dimensions;
+    uint32_t group;
+    DATATYPE tmp_distance;
+};
+
+// Structure to hold k-NN algorithm data, including k, best voters, and sample information
+struct KnnData {
+    uint32_t k;
+    struct Sample **best_voters;
+    struct Sample *samples[2];
+    uint32_t samples_count[2];
+    uint32_t samples_dimensions[2];
+};
+
+// Function to parse a string into a sample structure
+void parseStringToSample(struct Sample *sample, char *string, uint32_t max_dimensions, uint8_t has_group);
+
+// Function to parse samples from a file into the KnnData structure
+void parseFileToSamples(struct KnnData *knn, char *filepath);
+
+// Function to parse samples from the KnnData structure into a file (placeholder)
+void parseSamplesToFile(struct KnnData *knn, char *filepath);
+
+// Function to perform the k-NN algorithm
+void knnAlgorithm(struct KnnData *knn);
+
+// Function to sort the best voters in ascending order based on temporary distances
+void sortAscVoters(struct KnnData *knn);
+
+#endif //KNN_HEADER_H
