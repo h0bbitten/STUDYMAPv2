b27d1c1f7109ea8c94537bed03d304462e9b1a55
JonasPedersen123 <jopede23@student.aau.dk>
2023-12-05 22:57:06 +0100

    Far less hard coding, get wrecked kid

--- src/KNN.c
+++ src/KNN.c
@@ -3,33 +3,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define NUM_EDU 8
+#define NUM_ANSWER 8
 
 char* answers_path;
 char* datast_path = {"Databases/datast.csv"};
 
 typedef struct KnnDataPoints {
 
-    int answers[8];
+    int answers[NUM_ANSWER];
     char* name;
     double result;
 
 } KnnDataPoints;
 
-void parse_data (KnnDataPoints KnnTrainingPoint[8]);
-void parse_data_input (KnnDataPoints *KnnUserPoint);
+void parse_data(FILE* data_stream, KnnDataPoints* KnnDataPoint);
 double Euclidean_distance(KnnDataPoints KnnTrainingPoint, KnnDataPoints KnnUserPoint);
 int smallest_value(const void *a, const void *b);
+void cleanup(KnnDataPoints* KnnDataPoint);
 
 void knn() {
 
-    KnnDataPoints KnnTrainingPoint[8];
+    KnnDataPoints KnnTrainingPoint[NUM_EDU];
 
-    parse_data (KnnTrainingPoint);
+    FILE* datast;
+    datast = fopen(datast_path, "r");
+
+    for (int i = 0; i < NUM_EDU; ++i) {
+        parse_data(datast, &KnnTrainingPoint[i]);
+    }
+
+    fclose(datast);
+
+
+    FILE* answer;
+    //answer = fopen(answers_path, "r");
+    answer = fopen("Databases/Answers/Q.csv", "r");
 
     KnnDataPoints KnnUserPoint;
-    parse_data_input (&KnnUserPoint);
+    parse_data(answer, &KnnUserPoint);
 
-    for (int i = 0; i < 4; i++) {
+    fclose(answer);
+
+    for (int i = 0; i < NUM_EDU; i++) {
         KnnTrainingPoint[i].result = Euclidean_distance(KnnTrainingPoint[i], KnnUserPoint);
     }
 
@@ -43,69 +63,58 @@ void knn() {
         printf("%s: %f\n", KnnTrainingPoint[i].name, KnnTrainingPoint[i].result);
     }
 
+    // Clean up allocated memory
+    for (int i = 0; i < NUM_EDU; i++) {
+        cleanup(&KnnTrainingPoint[i]);
+    }
+    cleanup(&KnnUserPoint);
+
 }
 
-void parse_data (KnnDataPoints KnnTrainingPoint[8]) {
+void parse_data(FILE* data_stream, KnnDataPoints* KnnDataPoint) {
+    char line[1024];
+    char* token;
 
-    KnnTrainingPoint[0].name = "Computer Science";
-    KnnTrainingPoint[0].answers[0] = 3;
-    KnnTrainingPoint[0].answers[1] = 8;
-    KnnTrainingPoint[0].answers[2] = 10;
-    KnnTrainingPoint[0].answers[3] = 7;
-    KnnTrainingPoint[0].answers[4] = 10;
-    KnnTrainingPoint[0].answers[5] = 10;
-    KnnTrainingPoint[0].answers[6] = 8;
-    KnnTrainingPoint[0].answers[7] = 5;
-
-    KnnTrainingPoint[1].name = "Computer Technology";
-    KnnTrainingPoint[1].answers[0] = 3;
-    KnnTrainingPoint[1].answers[1] = 8;
-    KnnTrainingPoint[1].answers[2] = 10;
-    KnnTrainingPoint[1].answers[3] = 8;
-    KnnTrainingPoint[1].answers[4] = 7;
-    KnnTrainingPoint[1].answers[5] = 8;
-    KnnTrainingPoint[1].answers[6] = 8;
-    KnnTrainingPoint[1].answers[7] = 5;
-
-    KnnTrainingPoint[2].name = "Data Science and ML";
-    KnnTrainingPoint[2].answers[0] = 5;
-    KnnTrainingPoint[2].answers[1] = 6;
-    KnnTrainingPoint[2].answers[2] = 8;
-    KnnTrainingPoint[2].answers[3] = 10;
-    KnnTrainingPoint[2].answers[4] = 8;
-    KnnTrainingPoint[2].answers[5] = 8;
-    KnnTrainingPoint[2].answers[6] = 10;
-    KnnTrainingPoint[2].answers[7] = 7;
-
-    KnnTrainingPoint[2].name = "Information Technology";
-    KnnTrainingPoint[2].answers[0] = 8;
-    KnnTrainingPoint[2].answers[1] = 4;
-    KnnTrainingPoint[2].answers[2] = 7;
-    KnnTrainingPoint[2].answers[3] = 3;
-    KnnTrainingPoint[2].answers[4] = 6;
-    KnnTrainingPoint[2].answers[5] = 7;
-    KnnTrainingPoint[2].answers[6] = 5;
-    KnnTrainingPoint[2].answers[7] = 2;
-
-    for (int i = 0; i < 4; i++) {
-        KnnTrainingPoint[i].result = 0.0;
+    // Read a line from the CSV file
+    if (fgets(line, sizeof(line), data_stream) == NULL) {
+        // Handle error or end of file
+        return;
     }
 
-}
-void parse_data_input (KnnDataPoints *KnnUserPoint) {
+    // Tokenize the line using comma as a delimiter
+    token = strtok(line, ",");
+    int index = 0;
 
-    KnnUserPoint->name = "Allan";
-    KnnUserPoint->answers[0] = 3;
-    KnnUserPoint->answers[1] = 8;
-    KnnUserPoint->answers[2] = 10;
-    KnnUserPoint->answers[3] = 7;
-    KnnUserPoint->answers[4] = 10;
-    KnnUserPoint->answers[5] = 10;
-    KnnUserPoint->answers[6] = 8;
-    KnnUserPoint->answers[7] = 5;
+    while (token != NULL) {
+        if (strcmp(token, "done") == 0) {
+            break;  // Stop when reaching "done"
+        }
 
-    KnnUserPoint->result = 0.0;
+        // The first token is the name
+        if (index == 0) {
+            KnnDataPoint->name = malloc(strlen(token) + 1);  // +1 for the null terminator
+            if (KnnDataPoint->name != NULL) {
+                strcpy(KnnDataPoint->name, token);
+                token = strtok(NULL, ",");
+                token = strtok(NULL, ",");
+            }
+        } else if (index % 2 == 1) {
+            // Process only numeric answers and skip non-numeric values
+            if (isdigit(token[0])) {
+                KnnDataPoint->answers[(index - 1) / 2] = atoi(token);
+            }
+        }
+
+        // Move to the next token
+        token = strtok(NULL, ",");
+        index++;
+    }
+
+    // Initialize result
+    KnnDataPoint->result = 0.0;
+
 }
+
 double Euclidean_distance(KnnDataPoints KnnTrainingPoint, KnnDataPoints KnnUserPoint){
 
     double distance = 0.0;
@@ -124,4 +133,7 @@ int smallest_value(const void *a, const void *b) {
     if (result_A < result_B) return -1;
     else if (result_A > result_B) return 1;
     else return 0;
+}
+void cleanup(KnnDataPoints* KnnDataPoint) {
+    free(KnnDataPoint->name);
 }
