4f18a3a7e10ff9b658b165fc9e886127da5d4cb4
JonasPedersen123 <jopede23@student.aau.dk>
2023-12-07 12:34:21 +0100

    Fixed issue

--- src/STUDYMAP.c
+++ src/STUDYMAP.c
@@ -11,36 +11,18 @@ int main() {
     //The program boots and the user is prompted to login with "MY-ID"
     //Load_user(...)
     login();
-    /* TODO:
-     *  1. Unique username DOOOOOOOOOONE
-     *  2. Password hashing (Open SSL)
-     *  3. Tilføj "cpr-nummer" til bruger struct DOOOOOOOOONE
-    */
     //Add_user(...)
     //***OR***
     Load_profile();
 
-    //After getting the user profile, the program will get the education data from a remote database
+    //After getting the user profLile, the program will get the education data from a remote database
     //This is simulated by getting data from a csv file
     //Load_data(...)
-
-    /* TODO:
-     *  1. Mockup karakterblad ("CPR-nummer", karakter1...karakter_n, uddannelsested)
-     *  2. Hvis brugeren ikke har ungdomsuddannelse så lad bruger vælge en
-     *
-     */
-
 
     //Is there previously saved results?
     //***YES***
     //The user can now either start a new test or load previous results
     Questionnaire();
-    /* TODO:
-     *  1. Insert/update questions - Jonas!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-     *  2. Questionnaire progress
-     *  3. Dato og navn til questionnaire
-     *
-     *  */
 
     //KNN algorithm(...) - SØREN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
