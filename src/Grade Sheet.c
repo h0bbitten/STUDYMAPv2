2afdd80628a8058086c5a0b2cd85ee9fa09c5807
cvm08 <cmorsi23@student.aau.dk>
2023-12-08 15:27:42 +0100

    Search engine checking if a specific CPR number is in the Users.csv File.

--- src/Grade Sheet.c
+++ src/Grade Sheet.c
@@ -1,57 +1,58 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
+#include "data_collection.h"
+#include "stdio.h"
+#include "string.h"
 
-// Define a structure to hold user information
-struct User {
-    char cprNumber[11];  // Assuming CPR number is a string with 11 characters
-    char gradeSheet[1000];  // Assuming a string to hold grade sheet
-    char education[50];  // Assuming a string to hold education information
-};
+#define MAX_LINE_SIZE 112
 
-// Function to find a user by CPR number
-bool findUserByCPR(const char *cprNumber, const struct User users[], int numUsers, struct User *resultUser) {
-    for (int i = 0; i < numUsers; ++i) {
-        if (strcmp(users[i].cprNumber, cprNumber) == 0) {
-            *resultUser = users[i];
-            return true;  // User found
+typedef struct {
+    char username[50];
+    char password[50];
+    char cprNumber[11];
+} UserData;
+//Opens Users.csv in read-mode
+int searchUserByCPR(const char *cprNumber) {
+    FILE *file = fopen("databases/Users/Users.csv", "r");
+    if (!file) {
+        perror("Error opening file");
+        return -1; // File not found or unable to open
+    }
+
+    char line[MAX_LINE_SIZE];
+    while (fgets(line, sizeof(line), file) != NULL) {
+        UserData user;
+        sscanf(line, "%49[^,],%49[^,],%11s", user.username, user.password, user.cprNumber);
+
+        // Compare the CPR number
+        if (strcmp(user.cprNumber, cprNumber) == 0) {
+            printf("User Information:\n");
+            printf("Username: %s\n", user.username);
+            printf("CPR Number: %s\n", user.cprNumber);
+            fclose(file);
+            return 1; // Found the user
         }
     }
-    return false;  // User not found
+
+    fclose(file);
+    return 0; // CPR number not found
 }
+//search function
+int SearchCPR() {
+    char inputCPR[12];
 
-int main() {
-    // Create sample users
-    struct User users[3] = {
-            {"1111111111", "Dansk A, mdt.:07, Dansk A, skr.: 07, Engelsk A, mdt.: 07, Engelsk A, skr.:07, Mat A, mdt.: 07", "STX"},
-            {"2222222222", "Dansk A, mdt.:07, Dansk A, skr.: 07, Engelsk A, mdt.: 07, Engelsk A, skr.:07, Mat A, mdt.: 07", "HTX"},
-            {"3333333333", "Dansk A, mdt.:07, Dansk A, skr.: 07, Engelsk A, mdt.: 07, Engelsk A, skr.:07, Mat A, mdt.: 07",}
-    };
+    printf("Enter CPR number to search: ");
+    scanf("%10s", inputCPR);
 
-    // Ask the user to input the CPR number to search for
-    char searchCPR[11];
-    printf("Enter CPR number to search for: ");
-    scanf("%s", searchCPR);
-
-    struct User foundUser;
-
-    // Search for a user by CPR number
-    if (findUserByCPR(searchCPR, users, 3, &foundUser)) {
-        printf("User found:\n");
-        printf("CPR Number: %s\n", foundUser.cprNumber);
-        printf("Grade Sheet: %s\n", foundUser.gradeSheet);
-
-        if (strlen(foundUser.education) > 0) {
-            printf("Education: %s\n", foundUser.education);
-        } else {
-            // If education information is not available, ask the user to input it - dette indsætter ikke informationen i CSV filen - midlertidig løsning
-            printf("Education not available. Enter education: ");
-            scanf("%s", foundUser.education);
-            printf("Education: %s\n", foundUser.education);
-        }
-    } else {
-        printf("User with CPR Number %s not found\n", searchCPR);
+    int result = searchUserByCPR(inputCPR);
+//Error messages in case of CPR not being in the file or the file failing to open.
+    if (result == 0) {
+        printf("User with CPR number %s not found.\n", inputCPR);
+    } else if (result == -1) {
+        printf("Error occurred while opening the file.\n");
     }
 
     return 0;
+}
+
+int main() {
+    return SearchCPR();
 }
